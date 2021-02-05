cc3d6ceb33ce99a5fd18cae58db35ac81b58e494
octalsrc <nicklewchenko92@gmail.com>
2021-02-05 14:47:51 -0700

    State-ify SubCheck

--- haskell/src/Language/Carol/TypeCheck/SubCheck.hs
+++ haskell/src/Language/Carol/TypeCheck/SubCheck.hs
@@ -2,6 +2,7 @@ module Language.Carol.TypeCheck.SubCheck where
 
 import Data.SBV
 import Control.Monad.IO.Class
+import Control.Monad.State
 import Lens.Micro.Platform
 
 import Language.Carol.AST.Refinement
@@ -16,48 +17,50 @@ import Language.Carol.TypeCheck.Error
 subCheckV :: (RefDomain d)
   => ValT d
   -> ValT d
-  -> Context d
-  -> TErr d (Context d)
-subCheckV vt1 vt2 g = case (vt1,vt2) of
+  -> StateT (Context d) (TErr d) ()
+subCheckV vt1 vt2 = case (vt1,vt2) of
   -- InstantiateL
-  (ExVT a, vt2) -> onBase (instLV a vt2) g
+  (ExVT a, vt2) -> zoom base $ instLV a vt2
   -- InstantiateR+InstRSolve
-  (vt1, ExVT a) -> onBase (CB.bindExV a vt1) g
+  (vt1, ExVT a) -> base %>= CB.bindExV a vt1
   -- Unit, etc.
-  _ | vt1 == vt2 -> return g
+  _ | vt1 == vt2 -> return ()
   (PairT xt1 yt1, PairT xt2 yt2) -> do
-    g1 <- subCheckV xt1 xt2 g
-    g2 <- subCheckV yt1 yt2 g1
-    return g2
+    subCheckV xt1 xt2
+    subCheckV yt1 yt2
   (DsT t1 r1, DsT t2 r2) | t1 == t2 -> do
-    result <- liftIO . isTheorem $ do
+    g <- use base
+    result <- lift . liftIO . isTheorem $ do
       nu <- forall "nu"
       m <- CB.quantifyContext g
       return $ rpred m r1 nu .=> rpred m r2 nu
     if result
-       then return g
-       else terr $ TMismatch vt1 vt2
-  _ -> terr $ TMismatch vt1 vt2
+       then return ()
+       else lift.terr $ TMismatch vt1 vt2
+  _ -> lift.terr $ TMismatch vt1 vt2
 
 -- | Check that the first computation type is a subtype of the second
 -- computation type.
 subCheckC :: (RefDomain d)
   => CompT d
   -> CompT d
-  -> Context d
-  -> TErr d (Context d)
-subCheckC mt1 mt2 g = case (mt1,mt2) of
+  -> StateT (Context d) (TErr d) ()
+subCheckC mt1 mt2 = case (mt1,mt2) of
   -- Exvar
-  (ExCT b1,ExCT b2) | b1 == b2 -> return g
+  (ExCT b1,ExCT b2) | b1 == b2 -> return ()
   -- Pi elim
-  (mt1,Idx a s mt2') -> subCheckC mt1 mt2' (CB.idxBind a s g)
+  (mt1,Idx a s mt2') -> do
+    base %= CB.idxBind a s
+    subCheckC mt1 mt2'
   -- { InstantiateL, InstantiateR+InstRSolve, Unit, etc. }
-  (RetT vt1, RetT vt2) -> subCheckV vt1 vt2 g
+  (RetT vt1, RetT vt2) -> subCheckV vt1 vt2
   -- { <:--> }
   (FunT xt1 rt1, FunT xt2 rt2) -> do
-    g1 <- subCheckV xt1 xt2 g
-    rt1' <- CB.substC (g1^.base) rt1
-    rt2' <- CB.substC (g1^.base) rt2
-    subCheckC rt1' rt2' g1
+    subCheckV xt1 xt2
+    g <- use base
+    rt1' <- lift $ CB.substC g rt1
+    rt2' <- lift $ CB.substC g rt2
+    subCheckC rt1' rt2'
   -- InstantiateR (for all InstR rules except InstRSolve)
-  (mt,ExCT b) -> onBase (instRC mt b) g
+  -- (mt,ExCT b) -> onBase (instRC mt b) g
+  (mt,ExCT b) -> zoom base $ instRC mt b
